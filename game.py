e891f08fdeb7382e8657bac3afc41d2b0fd83d88
aikikode <aikikode@gmail.com>
2014-04-19 19:42:42 +0400

    Fix player movements after hitting the ball. Add ball indicator

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import random
 from cocos.director import director, cocos
 from cocos.layer import Layer, ColorLayer
 from cocos.scene import Scene
+import math
 from pyglet.window import key, pyglet
 import pymunk
 import constants
@@ -13,8 +15,8 @@ __author__ = 'aikikode'
 pyglet.resource.path = [constants.IMAGES_RESOURCE]
 pyglet.resource.reindex()
 
-SPEED = 800
-JUMP_SPEED = 900
+SPEED = constants.PLAYER_SPEED
+JUMP_SPEED = constants.PLAYER_JUMP_SPEED
 
 
 class Game(Layer):
@@ -24,18 +26,11 @@ class Game(Layer):
         super(Game, self).__init__()
         self.width, self.height = director.get_window_size()
         self.space = pymunk.Space()
-        self.space.gravity = (0.0, -900.0)
+        self.space.gravity = constants.GRAVITY
         # Background image
         bg = cocos.sprite.Sprite('background.png')
         bg.position = (self.width / 2., self.height / 2.)
         self.add(bg, z=-1)
-        #
-        self.create_container()
-        # Add ball
-        self.ball = Ball((self.width / 4., 400))
-        self.space.add(self.ball.body, self.ball.shape)
-        self.add(self.ball.sprite)
-        self.ball.body.sleep()
         # Add player 1
         self.player_1 = Player((200, 176/2), "player_1.png")
         self.space.add(self.player_1.body, self.player_1.body_shape, self.player_1.head_shape)
@@ -44,35 +39,62 @@ class Game(Layer):
         self.player_2 = Player((self.width - 200, 176/2), "player_2.png")
         self.space.add(self.player_2.body, self.player_2.body_shape, self.player_2.head_shape)
         self.add(self.player_2.sprite)
+        # Add ball
+        self.reset_ball()
+        # Add walls and net
+        self.create_container()
         #
         self.schedule(self.update)
         self.space.add_collision_handler(1, 2, begin=self.on_player_hits_wall, post_solve=self.on_player_hits_wall)
-        self.space.add_collision_handler(1, 4, begin=self.on_player_hits_wall, post_solve=self.on_player_hits_wall)
+        self.space.add_collision_handler(1, 4, begin=self.on_player_hits_net, post_solve=self.on_player_hits_net)
+        self.space.add_collision_handler(1, 5, begin=self.on_player_hits_net, post_solve=self.on_player_hits_net)
         self.space.add_collision_handler(1, 3, begin=self.on_player_hits_ground, post_solve=self.on_player_hits_ground)
         self.space.add_collision_handler(0, 1, begin=self.on_player_hits_ball)
         self.space.add_collision_handler(0, 4, begin=lambda x, y: False)
         self.space.add_collision_handler(0, 3, begin=self.on_ball_hits_ground)
         self.game_active = True
 
-    def reset_ball(self, player):
+    def reset_ball(self, player=None):
+        try:
+            self.space.remove(self.ball.body, self.ball.shape)
+            self.remove(self.ball.sprite)
+            self.remove(self.ball.indicator)
+        except AttributeError:
+            pass
+        # Add ball
+        if not player:
+            player = random.sample([self.player_1, self.player_2], 1)
         if player == self.player_1:
-            #self.ball.body.reset_forces()
-            #self.ball.body.velocity = (0, 0)
-            self.ball.body.position = ((self.width / 4., 400))
-            self.ball.body.sleep()
-        elif player == self.player_2:
-            #self.ball.body.reset_forces()
-            #self.ball.body.velocity = (0, 0)
-            self.ball.body.position = ((3 * self.width / 4., 400))
-            self.ball.body.sleep()
+            self.ball = Ball((self.width / 4., 400))
+        else:
+            self.ball = Ball((3 * self.width / 4., 400))
+        self.space.add(self.ball.body, self.ball.shape)
+        self.add(self.ball.sprite)
+        self.ball.body.sleep()
+        self.ball.set_indicator_height(self.height - 10)
+        self.add(self.ball.indicator)
+        self.player_1.reset((200, 176/2))
+        self.player_2.reset((self.width - 200, 176/2))
         self.game_active = True
 
     def on_player_hits_wall(self, space, arbiter):
         if arbiter.shapes[0] in [self.player_1.body_shape, self.player_1.head_shape]:
             self.player_1.body.velocity.x = 0
+            self.player_1.body.position.x = 65
         elif arbiter.shapes[0] in [self.player_2.body_shape, self.player_2.head_shape]:
             self.player_2.body.velocity.x = 0
+            self.player_2.body.position.x = self.width - 65
         return True
+
+    def on_player_hits_net(self, space, arbiter):
+        if arbiter.shapes[0] in [self.player_1.body_shape, self.player_1.head_shape]:
+            self.player_1.body.velocity.x = 0
+            self.player_1.body.position.x = self.width / 2 - 60 - 15
+        elif arbiter.shapes[0] in [self.player_2.body_shape, self.player_2.head_shape]:
+            self.player_2.body.velocity.x = 0
+            self.player_2.body.position.x = self.width / 2 + 60 + 15
+        return True
+
 
     def on_player_hits_ground(self, space, arbiter):
         if arbiter.shapes[0] in [self.player_1.body_shape]:
@@ -94,74 +116,80 @@ class Game(Layer):
         space = self.space
         # Add left and right screen borders
         ss = [
-            pymunk.Segment(space.static_body, (self.width, 0), (self.width, max(self.height * 10, 1000)), 5),
-            pymunk.Segment(space.static_body, (0, 0), (0, max(self.height * 10, 1000)), 5)
+            pymunk.Segment(space.static_body, (self.width, 0), (self.width, max(self.height * 10, 1000)), 15),
+            pymunk.Segment(space.static_body, (0, 0), (0, max(self.height * 10, 1000)), 15)
         ]
         for s in ss:
             s.elasticity = 0.95
             s.collision_type = 2
         space.add(ss)
         # Add ground
-        ground = pymunk.Segment(space.static_body, (0, 0), (self.width, 0), 5)
+        ground_body = space.static_body
+        ground = pymunk.Segment(ground_body, (0, 0), (self.width, 0), 15)
         ground.elasticity = 0.5
         ground.collision_type = 3
         ground.friction = 0.99
         space.add(ground)
         # Add net
-        net = pymunk.Segment(space.static_body, (self.width / 2., 0), (self.width / 2., 400), 5)
+        net = pymunk.Segment(space.static_body, (self.width / 2., 0), (self.width / 2., 400), 20)
         net.elasticity = 0.95
-        net.collision_type = 2
+        net.collision_type = 5
         space.add(net)
         net_sprite = cocos.sprite.Sprite('net.png')
         net_sprite.position = (self.width / 2., 200)
         self.add(net_sprite)
         # Add virtual net to prevent players from jumping to each other field
-        virtual_net = pymunk.Segment(space.static_body, (self.width / 2., 0), (self.width / 2., max(self.height * 10, 1000)), 5)
+        virtual_net = pymunk.Segment(space.static_body, (self.width / 2., 0), (self.width / 2., max(self.height * 10, 1000)), 20)
         virtual_net.elasticity = 0.95
         virtual_net.collision_type = 4
         space.add(virtual_net)
+        # Add simple motor to prevent players from sliding
+        self.space.add(pymunk.constraint.SimpleMotor(self.player_1.body, ground_body, 0))
+        self.space.add(pymunk.constraint.SimpleMotor(self.player_2.body, ground_body, 0))
 
     def update(self, dt):
-        dt = 1.0 / 80.
+        dt = 1.0 / 120.
         self.space.step(dt)
         self.ball.update(dt)
         self.player_1.update(dt)
         self.player_2.update(dt)
-        if not self.game_active and self.ball.body.velocity.y <= 5:
-            if self.ball.body.position[0] <= self.width / 2.:
-                self.reset_ball(self.player_2)
-            else:
-                self.reset_ball(self.player_1)
+        if not self.game_active:
+            if self.ball.body.position[0] >= self.width or self.ball.body.position[1] <= 0 or \
+                    (self.ball.body.velocity.y <= 10 and self.ball.body.velocity.get_length() <= 200):
+                if self.ball.body.position[0] <= self.width / 2.:
+                    self.reset_ball(self.player_2)
+                else:
+                    self.reset_ball(self.player_1)
 
     def on_key_press(self, k, m):
         if self.game_active:
             if k in (key.LEFT, key.RIGHT, key.UP):
                 if k == key.LEFT:
-                    self.player_2.body.velocity.x = -SPEED
+                    self.player_2.move_left()
                 elif k == key.RIGHT:
-                    self.player_2.body.velocity.x = SPEED
+                    self.player_2.move_right()
                 elif k == key.UP:
-                    if self.player_2.body.position[1] <= 95:
-                        self.player_2.body.velocity.y = JUMP_SPEED
+                    self.player_2.jump()
             elif k in (key.A, key.D, key.W):
                 if k == key.A:
-                    self.player_1.body.velocity.x = -SPEED
+                    self.player_1.move_left()
                 elif k == key.D:
-                    self.player_1.body.velocity.x = SPEED
+                    self.player_1.move_right()
                 elif k == key.W:
-                    if self.player_1.body.position[1] <= 95:
-                        self.player_1.body.velocity.y = JUMP_SPEED
+                    self.player_1.jump()
         return False
 
     def on_key_release(self, k, m):
         if k in (key.LEFT, key.RIGHT, key.UP):
-            if k == key.LEFT or k == key.RIGHT:
-                self.player_2.body.velocity.x = 0
+            if (k == key.LEFT and self.player_2.body.velocity.x < 0) or\
+                    (k == key.RIGHT and self.player_2.body.velocity.x > 0):
+                self.player_2.stop()
             elif k == key.UP:
                 pass
         elif k in (key.A, key.D, key.W):
-            if k == key.A or k == key.D:
-                self.player_1.body.velocity.x = 0
+            if (k == key.A and self.player_1.body.velocity.x < 0) or\
+                    (k == key.D and self.player_1.body.velocity.x > 0):
+                self.player_1.stop()
             elif k == key.W:
                 pass
         return False
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import cocos
+import math
 import pyglet
 import pymunk
 import constants
@@ -12,44 +13,76 @@ pyglet.resource.reindex()
 
 class Ball(object):
     def __init__(self, pos):
-        self.mass = mass = 100
-        self.radius = radius = 50
+        self.mass = mass = constants.BALL_MASS
+        self.radius = radius = constants.BALL_RADIUS
         inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
         self.body = pymunk.Body(mass, inertia)
         self.body.position = pos
-        self.body.velocity_limit = 1000
+        self.body.velocity_limit = constants.BALL_VELOCITY_LIMIT
+        self.body.apply_force(constants.BALL_ADDITIONAL_FORCE)
         shape = pymunk.Circle(self.body, self.radius, (0, 0))
-        shape.elasticity = 0.9
+        shape.elasticity = 0.95
         shape.friction = 0.9
         shape.collision_type = 0
         self.shape = shape
         self.sprite = cocos.sprite.Sprite('volleyball.png')
         self.sprite.scale = 5/6.
         self.sprite.position = pos
+        self.indicator = cocos.sprite.Sprite('ball_indicator.png')
+        self.indicator.position = (pos[0], 0)
+
+    def set_indicator_height(self, height):
+        self.indicator.position = (self.indicator.position[0], height)
 
     def update(self, dt):
         self.sprite.position = self.body.position
+        self.sprite.rotation = -self.body.angle * 180. / math.pi
+        self.indicator.position = (self.sprite.position[0], self.indicator.position[1])
 
 
 class Player(object):
     def __init__(self, pos, image):
         self.head_radius = 45
         self.body_radius = 60
-        self.body = pymunk.Body(100000, pymunk.inf)
+        self.body = pymunk.Body(constants.PLAYER_MASS, pymunk.inf)
         self.body.position = pos
-        self.body.velocity_limit = 900
+        self.body.apply_force(constants.PLAYER_ADDITIONAL_FORCE)
         head_shape = pymunk.Circle(self.body, self.head_radius, (0, 45))
         head_shape.layers = 0b001
         head_shape.collision_type = 1
-        head_shape.elasticity = 0.95
+        head_shape.elasticity = 0.99
+        head_shape.friction = 0.8
         self.head_shape = head_shape
         body_shape = pymunk.Circle(self.body, self.body_radius, (0, -30))
         body_shape.layers = 0b100
-        body_shape.elasticity = 0.95
+        body_shape.elasticity = 0.99
         body_shape.collision_type = 1
+        body_shape.friction = 0.9
         self.body_shape = body_shape
         self.sprite = cocos.sprite.Sprite(image)
         self.sprite.position = pos
 
     def update(self, dt):
         self.sprite.position = self.body.position
+
+    def move_left(self, speed=constants.PLAYER_SPEED):
+        self.body.velocity.x = -speed
+        self.body_shape.friction = 0
+
+    def move_right(self, speed=constants.PLAYER_SPEED):
+        self.body.velocity.x = speed
+        self.body_shape.friction = 0
+
+    def jump(self, speed=constants.PLAYER_JUMP_SPEED):
+        if self.body.position[1] <= 90 + 20:
+            self.body.velocity.y = speed
+
+    def stop(self):
+        self.body.velocity.x = 0
+        self.body_shape.friction = 1
+
+    def reset(self, pos):
+        self.body.position = pos
+        self.body.velocity = ((0, 0))
+        self.body.reset_forces()
+        self.body.apply_force(constants.PLAYER_ADDITIONAL_FORCE)
